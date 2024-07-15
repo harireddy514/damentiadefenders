3b80e8b5838daccb9748e0746934125a5c4c2e93
harireddy514 <136677730+harireddy514@users.noreply.github.com>
2024-07-15 19:10:12 +0530

    Update page.tsx

---
+++ client/src/app/personaldetails/page.tsx
@@ -0,0 +1,17 @@
+
+import PersonalDetails from '@/app/components/PersonalDetails'
+import Sidenav from '../components/Sidenav'
+
+const page = () => {
+  return (
+    <div>
+     <main className='ml-20'>
+      <div className='m-10'>
+     <PersonalDetails /> 
+     </div>
+     </main>
+    </div>
+  )
+}
+
+export default page
