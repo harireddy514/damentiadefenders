45bc33ac634f878c43448fdf2cad2b6fb9bfd2e9
harireddy514 <136677730+harireddy514@users.noreply.github.com>
2024-07-15 19:08:36 +0530

    Update page.tsx

---
+++ client/src/app/memorygames/guess/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import GuessGame from '@/app/components/GuessGame'
+const page = () => {
+  return (
+    <div>
+      <main className="ml-20">
+      <div className="m-10">
+      <GuessGame />
+      </div>
+      </main>
+    </div>
+  )
+}
+
+export default page
