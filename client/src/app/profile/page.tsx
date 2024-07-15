1e422061b3061823ec1350a10250e69e0961f792
harireddy514 <136677730+harireddy514@users.noreply.github.com>
2024-07-15 19:11:35 +0530

    Update page.tsx

---
+++ client/src/app/profile/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Profile from '../components/Profile'
+import Profile2 from '../components/Dashboard'
+const page = () => {
+  return (
+    <div>
+      {/* <Profile /> */}
+      <Profile2 />
+    </div>
+  )
+}
+
+export default page
