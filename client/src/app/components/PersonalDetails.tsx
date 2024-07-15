19eef53942f92aafa17015395c3114b5300ed5c1
harireddy514 <136677730+harireddy514@users.noreply.github.com>
2024-07-15 19:02:04 +0530

    Update PersonalDetails.tsx

---
+++ client/src/app/components/PersonalDetails.tsx
@@ -0,0 +1,15 @@
+'use client'
+import React from 'react'
+import Form from './Form';
+import { useState } from 'react';
+const PersonalDetails = () => {
+ 
+    return ( 
+        <div className='justify-center'>
+            <h3>Patient Details Form</h3>
+            <Form />
+     </div>
+    )
+}
+
+export default PersonalDetails
