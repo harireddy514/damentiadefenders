6a296963e19dc81f2c0629e6fa3c9e3a26cb28f7
harireddy514 <136677730+harireddy514@users.noreply.github.com>
2024-07-15 19:07:33 +0530

    Update page.tsx

---
+++ client/src/app/memories/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PhotoAlbum from '@/app/components/PhotoAlbum'
+import Sidenav from '../components/Sidenav'
+const page = () => {
+  return (
+    <div>
+      <PhotoAlbum />
+    </div>
+  )
+}
+
+export default page
