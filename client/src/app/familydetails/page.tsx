6ac1dd9a0b4522e4d48b9cdc78092d19319886e6
harireddy514 <136677730+harireddy514@users.noreply.github.com>
2024-07-15 19:06:43 +0530

    Update page.tsx

---
+++ client/src/app/familydetails/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Familydetails from '@/app/components/Familydetails'
+
+const page = () => {
+  return (
+    <div>
+      <main className="ml-20">
+      <div className='m-10'>
+     <Familydetails /> 
+     </div>
+     </main>
+    </div>
+  )
+}
+
+export default page
