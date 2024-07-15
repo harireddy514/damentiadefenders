f9ac1bf9d4c6aa50e0fc80e2d4d74c25f77d406c
harireddy514 <136677730+harireddy514@users.noreply.github.com>
2024-07-15 19:06:08 +0530

    Update page.tsx

---
+++ client/src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import Dashboard from '../components/Dashboard'
+import Sidenav from '../components/Sidenav'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  )
+}
+
+export default page
