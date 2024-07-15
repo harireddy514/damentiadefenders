ed0a2dfcbca98824ae1dcbd896db535ec5424372
harireddy514 <136677730+harireddy514@users.noreply.github.com>
2024-07-15 19:07:07 +0530

    Update page.tsx

---
+++ client/src/app/login/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Login from '../components/Login'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+const page = () => {
+  return (
+    <div>
+      <Header />
+      <Login />
+    </div>
+  )
+}
+
+export default page
