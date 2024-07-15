9311ea07ebabedd9e1e80a4fe65d6ed7d5aed992
harireddy514 <136677730+harireddy514@users.noreply.github.com>
2024-07-15 18:56:23 +0530

    Update Footer.tsx

---
+++ client/src/app/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='fixed inset-x-0 bottom-0 text-white p-3 w-full font-bold bg-blue-950 flex justify-between '>
+      <h5>Copyright @ 2024</h5>
+    </div>
+  )
+}
+
+export default Footer
