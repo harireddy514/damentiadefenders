1057b6901b5e476f31f6cab9dc558547735931d5
harireddy514 <136677730+harireddy514@users.noreply.github.com>
2024-07-15 19:09:41 +0530

    Update page.tsx

---
+++ client/src/app/memorygames/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import MemoryGames from '../components/MemoryGames'
+import Sidenav from '../components/Sidenav'
+
+const page = () => {
+  return (
+    <div>
+      <main className="ml-20">
+        <div className="m-10">
+      <MemoryGames />
+      </div>
+      </main>
+    </div>
+  )
+}
+
+export default page
