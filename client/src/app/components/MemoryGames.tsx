dc37596cc0d23e7733bfeecc538ed2b48126bad5
harireddy514 <136677730+harireddy514@users.noreply.github.com>
2024-07-15 19:01:41 +0530

    Update MemoryGames.tsx

---
+++ client/src/app/components/MemoryGames.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Game from './Game'
+const MemoryGames = () => {
+  return (
+    <div>
+      <h2  className='flex justify-center mt-3'>Memory Games</h2>
+      <h4 className='flex justify-center mt-3'>Improve Your Cognitive Ability !!</h4>
+      <Game name="Recognize who?" link="guess"/>
+      <Game name="Personalized Quiz" link=""/>
+    </div>
+  )
+}
+
+export default MemoryGames
