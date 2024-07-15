74c16b182a56c8bfaa91526546fb1b1b37440c09
harireddy514 <136677730+harireddy514@users.noreply.github.com>
2024-07-15 19:05:35 +0530

    Update Task.tsx

---
+++ client/src/app/components/Task.tsx
@@ -0,0 +1,28 @@
+'use client'
+import React, { useState, useEffect } from 'react'
+import Button from '@mui/material/Button';
+import CheckIcon from '@mui/icons-material/Check';
+const Task = (props) => {
+  const [isToggle, setIsToggle]=useState(false);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsToggle(false);
+    }, 24 * 60 * 60 * 1000); 
+
+    // Clear timeout if the component is unmounted
+    return () => clearTimeout(timer);
+  }, []);
+  return (
+    <div  style={(isToggle) ? {backgroundColor:'lightblue'} :{backgroundColor:'white'} }>
+      <div className='flex justify-between align-center'>
+      <h4 style={(isToggle) ? {backgroundColor:'lightblue'} :{backgroundColor:'white'} } className='text-blue-800'>{props.task}</h4>
+      <Button variant="outlined" startIcon={<CheckIcon />} className='float-right mr-2' onClick={()=>{setIsToggle(!isToggle)}}>
+        Complete
+      </Button>
+      </div>
+      <h6 className='text-blue-700'>{props.time}</h6>
+    </div>
+  )
+}
+
+export default Task
