fc12131956029ee4f06434299e42cf1da7206473
harireddy514 <136677730+harireddy514@users.noreply.github.com>
2024-07-15 19:02:29 +0530

    Update PhotoAlbum.tsx

---
+++ client/src/app/components/PhotoAlbum.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+const PhotoAlbum = () => {
+  function srcset(image: string, size: number, rows = 1, cols = 1) {
+    return {
+      src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
+      srcSet: `${image}?w=${size * cols}&h=${
+        size * rows
+      }&fit=crop&auto=format&dpr=2 2x`,
+    };
+  }
+  return (
+    <div>
+      <ImageList
+      sx={{ width: 500, height: 450 }}
+      variant="quilted"
+      cols={4}
+      rowHeight={121}
+    >
+      {itemData.map((item) => (
+        <ImageListItem key={item.img} cols={item.cols || 1} rows={item.rows || 1}>
+          <img
+            {...srcset(item.img, 121, item.rows, item.cols)}
+            alt={item.title}
+            loading="lazy"
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
+    </div>
+  )
+}
+
+export default PhotoAlbum
