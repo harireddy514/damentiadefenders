1826128c3ce677ccac41c95517cae9579658669a
harireddy514 <136677730+harireddy514@users.noreply.github.com>
2024-07-15 19:12:09 +0530

    Update page.tsx

---
+++ client/src/app/register/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Register from '../components/Register'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+const page = () => {
+  return (
+    <div>
+      <Header />
+      <Register />
+      <Footer />
+    </div>
+  )
+}
+
+export default page
