7ae878130f1f51dc33933fa8ed3aaf34b340ef22
harireddy514 <136677730+harireddy514@users.noreply.github.com>
2024-07-15 19:14:34 +0530

    Update questions.js

---
+++ client/src/app/questions.js
@@ -0,0 +1,35 @@
+const questions = [
+    {
+        text: "When were you born?"
+    },
+    {
+        text: "What are the names of your children?"
+    },
+       {
+        text: "Tell us about one of your good memories."
+    },
+       {
+        text: "What are your hobbies?"
+    },
+       {
+        text: "Where do you stay?"
+    },
+       {
+        text: "What is your age?"
+    },
+       {
+        text: "What are the hobbies of your {}?"
+    },
+    {
+        text: "When is the birthday of your {}?"
+    },
+    {
+        text: "Where does your {} stay?"
+    },
+    {
+        text: "What is the nam of your {}?"
+    },
+    // {
+    //     text: "When were you born?"
+    // }
+]
