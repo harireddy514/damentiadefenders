0b6abdb4ff97b8d61543693caa9848a27ce1ff6e
harireddy514 <136677730+harireddy514@users.noreply.github.com>
2024-07-15 19:09:20 +0530

    Update page.tsx

---
+++ client/src/app/memorygames/quiz/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Quiz from '@/app/components/Quiz'
+const page = () => {
+  return (
+    <div>
+     <main className="ml-20">
+      <div className="m-10">
+      <Quiz/>
+      </div>
+      </main> 
+    </div>
+  )
+}
+
+export default page
